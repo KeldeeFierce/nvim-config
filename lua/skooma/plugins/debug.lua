51472899afd98ac8c480f73a53330e2be156f464
KeldeeFierce <keller.dm@yandex.ru>
2024-08-18 05:50:40 +0300

    debug cpp test

--- lua/skooma/plugins/debug.lua
+++ lua/skooma/plugins/debug.lua
@@ -20,7 +20,43 @@ return {
 			dapui.close()
 		end
 
-		require("dap-python").setup("python3")
+		local py_db_path = "/home/dmitry/.local/share/nvim/mason/packages/debugpy/venv/bin/python3"
+		-- local cpp_db_path =
+		-- "/home/dmitry/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7" --cpptools
+
+		-- dap.adapters.cppdbg = {
+		-- 	id = "cppdbg",
+		-- 	type = "executable",
+		-- 	command = cpp_db_path,
+		-- }
+
+		-- dap.configurations.cpp = {
+		-- 	{
+		-- 		name = "Launch file",
+		-- 		type = "cppdbg",
+		-- 		request = "launch",
+		-- 		program = function()
+		-- 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		-- 		end,
+		-- 		cwd = "${workspaceFolder}",
+		-- 		stopAtEntry = true,
+		-- 	},
+		-- 	{
+		-- 		name = "Attach to gdbserver :1234",
+		-- 		type = "cppdbg",
+		-- 		request = "launch",
+		-- 		MIMode = "gdb",
+		-- 		miDebuggerServerAddress = "localhost:1234",
+		-- 		miDebuggerPath = "/usr/bin/gdb",
+		-- 		cwd = "${workspaceFolder}",
+		-- 		program = function()
+		-- 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		-- 		end,
+		-- 	},
+		-- }
+
+		-- require("dap-python").setup("python3") --Use local debugger
+		require("dap-python").setup(py_db_path) --Use Mason debugger
 		require("dapui").setup()
 		--To install manually:
 		-- dap.adapters.python = function(cb, config)
--- lua/skooma/plugins/mason.lua
+++ lua/skooma/plugins/mason.lua
@@ -51,6 +51,7 @@ return {
 				"eslint_d",
 				"shellcheck",
 				"luacheck",
+				"debugpy",
 			},
 		})
 	end,
