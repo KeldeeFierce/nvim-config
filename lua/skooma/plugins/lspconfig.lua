603e491789bd7ef9863694015b864d06f61d4f03
KeldeeFierce <keller.dm@yandex.ru>
2026-03-23 14:45:22 +0300

    add lspconfig for go

--- lua/skooma/plugins/lspconfig.lua
+++ lua/skooma/plugins/lspconfig.lua
@@ -104,6 +104,28 @@ return {
 			},
 		})
 
+		require("mason-lspconfig").setup({
+			handlers = {
+				function(server_name)
+					local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+					vim.lsp.config(server_name, {
+						capabilities = capabilities,
+					})
+
+					vim.lsp.enable(server_name)
+				end,
+				["jedi_language_server"] = function()
+					-- configure bash language server
+					lspconfig["jedi_language_server"].setup({
+						capabilities = capabilities,
+						filetypes = { "python" },
+					})
+				end,
+			},
+		})
+
+		--		--
 		-- mason_lspconfig.setup_handlers({
 		-- 	-- default handler for installed servers
 		-- 	function(server_name)
@@ -242,5 +264,59 @@ return {
 		-- 		})
 		-- 	end,
 		-- })
+		--
+
+		vim.lsp.config("lua_ls", {
+			on_init = function(client)
+				if client.workspace_folders then
+					local path = client.workspace_folders[1].name
+					if
+						path ~= vim.fn.stdpath("config")
+						and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+					then
+						return
+					end
+				end
+
+				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+					runtime = {
+						-- Tell the language server which version of Lua you're using (most
+						-- likely LuaJIT in the case of Neovim)
+						version = "LuaJIT",
+						-- Tell the language server how to find Lua modules same way as Neovim
+						-- (see `:h lua-module-load`)
+						path = {
+							"lua/?.lua",
+							"lua/?/init.lua",
+						},
+					},
+					-- Make the server aware of Neovim runtime files
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+							-- Depending on the usage, you might want to add additional paths
+							-- here.
+							-- '${3rd}/luv/library',
+							-- '${3rd}/busted/library',
+						},
+						-- Or pull in all of 'runtimepath'.
+						-- NOTE: this is a lot slower and will cause issues when working on
+						-- your own configuration.
+						-- See https://github.com/neovim/nvim-lspconfig/issues/3189
+						-- library = vim.api.nvim_get_runtime_file('', true),
+					},
+				})
+			end,
+			settings = {
+				Lua = {},
+			},
+		})
+
+		vim.lsp.config("gopls", {
+			capabilities = capabilities,
+		})
+
+		vim.lsp.enable("gopls")
 	end,
 }
