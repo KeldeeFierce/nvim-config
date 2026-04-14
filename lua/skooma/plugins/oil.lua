710c4941806fab72c300771b155aaf06c2d0f904
KeldeeFierce <keller.dm@yandex.ru>
2026-04-15 00:50:05 +0300

    add git signs to oil

--- lua/skooma/plugins/oil.lua
+++ lua/skooma/plugins/oil.lua
@@ -1,23 +1,38 @@
 return {
-	"stevearc/oil.nvim",
+	{
+		"stevearc/oil.nvim",
 
-	config = function()
-		require("oil").setup({
-			view_options = {
-				-- Show files and directories that start with "."
-				show_hidden = true,
-				-- This function defines what is considered a "hidden" file
-				is_hidden_file = function(name, bufnr)
-					local m = name:match("^%.")
-					return m ~= nil
-				end,
-				-- This function defines what will never be shown, even when `show_hidden` is set
-				is_always_hidden = function(name, bufnr)
-					return false
-				end,
-			},
-		})
+		config = function()
+			require("oil").setup({
+				view_options = {
+					-- Show files and directories that start with "."
+					show_hidden = true,
+					-- This function defines what is considered a "hidden" file
+					is_hidden_file = function(name, bufnr)
+						local m = name:match("^%.")
+						return m ~= nil
+					end,
+					-- This function defines what will never be shown, even when `show_hidden` is set
+					is_always_hidden = function(name, bufnr)
+						return false
+					end,
+				},
+				win_options = {
+					signcolumn = "yes:2",
+				},
+			})
 
-		vim.keymap.set("n", "<leader>oo", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-	end,
+			vim.keymap.set("n", "<leader>oo", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+			vim.keymap.set("n", "<leader>oi", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+		end,
+	},
+	{
+		"refractalize/oil-git-status.nvim",
+
+		dependencies = {
+			"stevearc/oil.nvim",
+		},
+
+		config = true,
+	},
 }
