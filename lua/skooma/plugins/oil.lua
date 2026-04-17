10d33d9141c43b2654ee7c987b7555ca7b3e5056
KeldeeFierce <keller.dm@yandex.ru>
2026-04-17 09:04:44 +0300

    set oil as default file browser

--- lua/skooma/plugins/oil.lua
+++ lua/skooma/plugins/oil.lua
@@ -23,7 +23,7 @@ return {
 			})
 
 			vim.keymap.set("n", "<leader>oo", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-			vim.keymap.set("n", "<leader>oi", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+			vim.keymap.set("n", "<leader>ee", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
 		end,
 	},
 	{
