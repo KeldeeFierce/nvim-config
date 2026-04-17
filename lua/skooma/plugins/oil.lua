619e99096e8386814802d8ccf4d70abc1aeff64c
KeldeeFierce <keller.dm@yandex.ru>
2026-04-17 23:40:56 +0300

    change oil hotkeys

--- lua/skooma/plugins/oil.lua
+++ lua/skooma/plugins/oil.lua
@@ -22,8 +22,8 @@ return {
 				},
 			})
 
-			vim.keymap.set("n", "<leader>oo", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-			vim.keymap.set("n", "<leader>ee", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+			vim.keymap.set("n", "<leader>ee", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+			vim.keymap.set("n", "<leader>oi", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
 		end,
 	},
 	{
