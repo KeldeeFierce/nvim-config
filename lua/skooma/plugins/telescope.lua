fa8dc3d1a5b5eb4e4e8a5e6b1d7a85dc6e420f8a
KeldeeFierce <keller.dm@yandex.ru>
2026-05-08 14:19:02 +0300

    fix treesitter setup after swithching to main branch

--- lua/skooma/plugins/telescope.lua
+++ lua/skooma/plugins/telescope.lua
@@ -24,6 +24,9 @@ return {
 		--
 		telescope.setup({
 			defaults = {
+				preview = {
+					treesitter = false,
+				},
 				path_display = { "smart" },
 				mappings = {
 					i = {
--- lua/skooma/plugins/treesitter.lua
+++ lua/skooma/plugins/treesitter.lua
@@ -7,57 +7,84 @@ return {
 	},
 	config = function()
 		-- import nvim-treesitter plugin
-		local treesitter = require("nvim-treesitter.configs")
+		local treesitter = require("nvim-treesitter")
 
 		-- configure treesitter
 		treesitter.setup({
-			-- enable syntax highlighting
-			highlight = {
-				enable = true,
-				disable = { "dockerfile" },
-			},
-			-- enable indentation
-			indent = { enable = true, disable = { "python" } },
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = {
-				enable = true,
-			},
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"prisma",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-				"vimdoc",
-				"c",
-				"python",
-				"go",
-				"rust",
-				"sql",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
+			install_dir = vim.fn.stdpath("data") .. "/site",
+
+			-- -- enable syntax highlighting
+			-- highlight = {
+			-- 	enable = true,
+			-- 	disable = { "dockerfile" },
+			-- },
+			-- -- enable indentation
+			-- indent = { enable = true, disable = { "python" } },
+			-- -- enable autotagging (w/ nvim-ts-autotag plugin)
+			-- autotag = {
+			-- 	enable = true,
+			-- },
+			-- -- ensure these language parsers are installed
+			-- incremental_selection = {
+			-- 	enable = true,
+			-- 	keymaps = {
+			-- 		init_selection = "<C-space>",
+			-- 		node_incremental = "<C-space>",
+			-- 		scope_incremental = false,
+			-- 		node_decremental = "<bs>",
+			-- 	},
+			-- },
 		})
+
+		treesitter.install({
+			"json",
+			"javascript",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+			"prisma",
+			"markdown",
+			"markdown_inline",
+			"svelte",
+			"graphql",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"query",
+			"vimdoc",
+			"c",
+			"python",
+			"go",
+			"rust",
+			"sql",
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				pcall(vim.treesitter.start, args.buf)
+			end,
+		})
+
+		--Disable some filetypes, shoud try it later
+		--
+		-- vim.api.nvim_create_autocmd("FileType", {
+		-- 	callback = function(args)
+		-- 		local ft = vim.bo[args.buf].filetype
+		--
+		-- 		local disabled = {
+		-- 			dockerfile = true,
+		-- 		}
+		--
+		-- 		if disabled[ft] then
+		-- 			return
+		-- 		end
+		--
+		-- 		pcall(vim.treesitter.start, args.buf)
+		-- 	end,
+		-- })
 	end,
 }
